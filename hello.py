3cc57fbb265beef890c05af6e7c270f59f67814b
al-bratkov <97631318+al-bratkov@users.noreply.github.com>
2022-01-23 23:48:24 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-def serv():
-  pass
+def serv(environ, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  start_response(status, headers)
+  print(environ)
+  #body = str
+  return body
